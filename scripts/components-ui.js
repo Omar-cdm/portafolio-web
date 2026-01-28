e9db1ec43001ec2eaea98ff2dcdbe0432aba51b4
omar-dev <omarjunior052202@gmail.com>
2026-01-27 21:50:46 -0500

    Ventana de politica y privacidad lista.

--- scripts/components-ui.js
+++ scripts/components-ui.js
@@ -204,9 +204,9 @@ export function footer() {
                 <div class="flex text-xs mb-1 sm:col-span-3 sm:w-full sm:gap-6 sm:mt-4 footer__credits">
                     <button popovertarget="info-privacidad" class="w-1/2 sm:text-end underline hover:cursor-pointer footer__credits-link">Política de Privacidad</button>
 
-                    <div id="info-privacidad" popover class="footer__credits-dialog">
-                        <h3 class="credits__dialog-title">Política de Privacidad</h3>
-                        <p>
+                    <div id="info-privacidad" popover class="bg-white/2 backdrop-blur-md p-4 border-x-2 border-green-400 rounded-md inset-x-1/5 top-30 sm:inset-x-1/3 footer__credits-dialog">
+                        <h2 class="text-lg text-green-dark text-center font-bold my-2 sm:text-xl credits__dialog-title">Política de Privacidad</h3>
+                        <p class="text-xs text-white-green text-center mb-2 sm:text-base">
                         Uso <strong>Formspree</strong> para recibir tus correos. No guardo ni vendo tus datos.
                         Al usar el formulario de la sección contacto, aceptas este flujo de comunicación técnica con <strong>Formspree</strong>. 
                         </p>
@@ -223,4 +223,3 @@ export function footer() {
         </footer>
     `;
 };
-//
