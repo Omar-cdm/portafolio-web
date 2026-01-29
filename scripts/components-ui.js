ae239369ed3677f64cf6496168cf87b0f90709de
omar-dev <omarjunior052202@gmail.com>
2026-01-28 19:21:42 -0500

    Corrección en medidas de font-size para responsive desktop.

--- scripts/components-ui.js
+++ scripts/components-ui.js
@@ -9,7 +9,7 @@ export const newprojectsElements = projectsElements.join("");
 
 export function header() {
     return `
-        <header class="w-5/6 h-14 bg-white/2 backdrop-blur-sm sticky z-1 top-1 inset-x-1/12 flex justify-between items-center border-x-1 border-y-1 border-green-400 border-b-green-dark rounded-tl-xl rounded-br-xl px-4 py-2 animate-entrance header">
+        <header class="w-5/6 h-14 bg-white/2 backdrop-blur-sm sticky z-1 top-1 inset-x-1/12 flex justify-between items-center border-x-1 border-y-1 border-green-400 border-b-green-dark rounded-tl-xl rounded-br-xl px-4 py-2 animate-entrance sm:h-15 header">
 
             <div class="w-12 h-1 absolute bottom-0 left-8 bg-green-dark"></div>
             <div class="w-14 h-1 absolute top-0 right-0 bg-green-400"></div>
@@ -34,16 +34,16 @@ export function header() {
 
 function navbar() {
     return `
-        <nav class="w-27 hidden bg-black-bluish text-white-green p-2 rounded-md border-1 border-green-400 absolute -top-1 -right-1  menu__nav" id="menu-desplegable">
+        <nav class="w-27 hidden bg-black-bluish text-white-green p-2 rounded-md border-1 border-green-400 absolute -top-1 -right-1 lg:w-32  menu__nav" id="menu-desplegable">
             <ul class="text-sm my-2 flex flex-col items-center gap-3 menu__list">
                 <li>
-                    <a href="./" class="px-1 border-1 border-b-3 border-r-3 border-green-400 link">Inicio</a>        
+                    <a href="./" class="px-1 border-1 border-b-3 border-r-3 border-green-400 lg:px-2 lg:text-lg link">Inicio</a>        
                 </li>
                 <li>
-                    <a href="./" class="px-1 border-1 border-b-3 border-r-3 border-green-400 link">Proyectos</a>
+                    <a href="./" class="px-1 border-1 border-b-3 border-r-3 border-green-400 lg:px-2 lg:text-lg link">Proyectos</a>
                 </li>
                 <li>
-                    <a href="./" class="px-1 border-1 border-b-3 border-r-3 border-green-400 link">Contacto</a>
+                    <a href="./" class="px-1 border-1 border-b-3 border-r-3 border-green-400 lg:px-2 lg:text-lg link">Contacto</a>
                 </li>
             </ul>
 
@@ -72,9 +72,9 @@ export function elementHeroContainer() {
                 </div>
                 <div class="w-full bg-white/2 backdrop-blur-sm flex flex-col justify-center p-4 border-y-2 border-green-400 rounded-tl-xl rounded-br-xl relative lg:w-5/6 lg:h-1/3  hero__content">
                     <div class="w-1/3 h-3 bg-green-400 rounded-b-md absolute top-0 left-6"></div>
-                    <h1 class="w-full text-3xl text-end text-green-400 text-center font-bold py-1 mb-2 hero__title">Omar Rodriguez</h1>
-                    <p class="pb-5 hero__description">Desarrollador web en formación, con mucho amor por la tecnología y el software.</p>            
-                    <button class="text-sm text-black-bluish font-semibold absolute bottom-0 right-8 bg-green-400 rounded-t-xl px-4 py-1 hover:cursor-pointer button-project">Proyectos</button>
+                    <h1 class="w-full text-3xl text-end text-green-400 text-center font-bold py-1 mb-2 sm:text-4xl hero__title">Omar Rodriguez</h1>
+                    <p class="pb-5 sm:text-lg hero__description">Desarrollador web en formación, con mucho amor por la tecnología y el software.</p>            
+                    <button class="text-sm text-black-bluish font-semibold absolute bottom-0 right-8 bg-green-400 rounded-t-xl px-4 py-1 sm:text-base hover:cursor-pointer button-project">Proyectos</button>
                 </div>
             </div>
     `;
@@ -88,9 +88,9 @@ export function elementBlockquote() {
                 <div class="size-10 absolute -top-1 -right-1 border-t-3 border-r-3 border-green-400"></div>
                 <div class="size-10 absolute -bottom-1 -left-1 border-b-3 border-l-3 border-green-400"></div>
                 <div class="size-10 absolute -bottom-1 -right-1 border-b-3 border-r-3 border-green-400"></div>
-                <h2 class="text-center text-2xl font-extrabold my-2 lg:text-xl lg:my-2 quote__subtitle">¡NUNCA PARES DE APRENDER!</h2>
+                <h2 class="text-center text-2xl font-extrabold my-2 lg:text-xl lg:my-1 quote__subtitle">¡NUNCA PARES DE APRENDER!</h2>
 
-                <p class="text-center text-sm quote__text">
+                <p class="text-center text-sm lg:text-base quote__text">
                     Esta frase muy sonada en la comunidad de Platzi no solo me ha motivado, también me ha enseñado a ser constante con el aprendizaje, creando en mí el hábito de siempre querer aprender.
                 </p>
                 
@@ -103,10 +103,10 @@ export function elementBlockquote() {
 export function elementLiValue(valueName) {
     return `
         <li class="mb-3 values__item">
-            <h4 class="font-semibold mb-1">${valueName}</h4>
+            <h4 class="font-semibold mb-1 lg:text-lg">${valueName}</h4>
             <div class="ml-2 flex items-center">
               <img src="./assets/arrow-2.svg" class="size-2"/>
-              <div class="w-40 h-3 bg-green-400 mx-1"></div>
+              <div class="w-40 h-3 bg-green-400 mx-1 lg:w-50 lg:h-4"></div>
               <p class="text-green-dark">100%</p>
         </li>
     `;
@@ -128,7 +128,7 @@ function elementArticle(title, img, demo, codigo) {
     return `
         <article class="w-5/6 p-2 bg-white/2 backdrop-blur-sm grid place-items-center border-1 border-b-2 border-green-400 rounded-xl lg:max-w-3/4 project">
             <div class="h-7 absolute -top-4 left-0 bg-green-400 text-black-bluish px-2 rounded-t-md rounded-tr-xl">
-                <h2 class="text-center text-base font-semibold pr-1 values__title">${title}</h2>
+                <h2 class="text-center text-base font-semibold pr-1 lg:text-lg values__title">${title}</h2>
             </div>
             <p class="project__description">
             </p>
@@ -136,12 +136,12 @@ function elementArticle(title, img, demo, codigo) {
             <div class="w-5/6 mt-4 project__container-image"><img class="project__image" src="${img}"></div>
 
             <div class="w-full px-1 flex gap-3 justify-end my-3 project__technologies">
-                <a class="text-xs flex items-center gap-1 p-1 border-1 border-r-3 border-b-3 border-green-400 project__technologies--demo" href="${demo}" target="_blank">
+                <a class="text-xs flex items-center gap-1 p-1 border-1 border-r-3 border-b-3 border-green-400 lg:text-sm project__technologies--demo" href="${demo}" target="_blank">
                     <svg class="size-4 fill-green-dark technologies--icon-demo" viewBox="0 0 24 24" id="curve-arrow-right-9" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color"><path id="secondary" d="M21.71,8.29l-3-3a1,1,0,0,0-1.42,1.42L18.59,8H16.71a11.78,11.78,0,0,0-10.6,6.55,1,1,0,0,0,.44,1.34A.93.93,0,0,0,7,16a1,1,0,0,0,.89-.55A9.81,9.81,0,0,1,16.71,10h1.88l-1.3,1.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l3-3A1,1,0,0,0,21.71,8.29Z"></path><path id="primary" d="M20,21H4a2,2,0,0,1-2-2V5A2,2,0,0,1,4,3h8a1,1,0,0,1,0,2H4V19H20V16a1,1,0,0,1,2,0v3A2,2,0,0,1,20,21Z"></path></svg>
 
                     Demo
                 </a>
-                <a class="text-xs flex items-center p-1 gap-1 border-1 border-r-3 border-b-3 border-green-400 project__technologies--codigo" href="${codigo}" target="_blank">
+                <a class="text-xs flex items-center p-1 gap-1 border-1 border-r-3 border-b-3 border-green-400 lg:text-sm project__technologies--codigo" href="${codigo}" target="_blank">
                     <svg class="size-4 fill-green-dark technologies--icon-codigo" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1873.256 674.212c0-122.468-35.728-230.622-106.325-321.88 15.712-57.683 32.5-171.972-20.77-313.703-5.596-15.066-17.864-26.796-33.146-32.07-19.155-6.564-122.144-30.347-328.552 110.953-84.157-22.169-172.94-33.576-264.844-34.114-91.582.538-180.366 11.945-264.306 34.114C648.689-23.68 545.7-.005 526.437 6.56c-15.282 5.165-27.55 17.003-33.146 32.177-53.162 141.623-36.374 255.912-20.555 313.594-70.704 91.582-106.54 199.629-106.54 321.88 0 422.826 221.905 557.131 432.188 605.666-46.49 50.903-95.24 125.158-105.68 219.537-71.78 26.044-257.633 78.13-325.109-44.015-3.013-5.596-73.394-137.641-215.017-148.08-25.075 1.184-84.587 4.305-101.052 54.346-18.51 55.961 39.495 97.716 67.906 117.41.538.323 55.745 31.962 93.841 132.045.969 4.628 23.783 114.72 134.951 181.55 87.492 52.731 202.319 63.493 341.252 32.284 0 23.46-.323 48.212-.43 63.386l-.324 31.101H796.34l.215-29.81c1.937-161.532 2.045-164.653-30.778-181.656-12.484-6.457-27.012-7.964-40.356-3.659-136.243 41.002-246.98 40.033-320.052-2.798-70.488-41.54-85.985-108.908-88.89-119.67-3.875-10.653-8.072-20.662-12.269-30.24 126.987 145.713 372.138 77.161 463.289 33.792 18.833-8.825 30.778-27.765 30.778-48.535 0-149.372 148.295-244.182 149.802-245.15 19.371-12.16 28.949-35.19 23.998-57.36-4.95-22.276-23.352-39.065-46.06-41.755-223.626-26.796-452.204-109.338-452.204-518.388 0-106.325 33.577-198.983 99.76-275.175 12.915-15.067 16.681-36.052 9.579-54.562-9.793-25.397-36.482-112.782-.323-234.819 35.729 4.09 110.845 24.321 231.16 110.307 13.344 9.578 30.455 12.376 46.383 7.856 80.497-23.568 170.356-36.267 259.248-36.805 89.321.538 179.073 13.237 259.786 36.805 15.927 4.52 32.93 1.722 46.382-7.856 121.176-86.631 196.4-106.433 230.73-110.63 36.482 122.145 9.793 209.745 0 235.142-7.21 18.51-3.444 39.603 9.578 54.562 66.076 75.977 99.545 168.527 99.545 275.175 0 409.157-229.115 491.161-453.173 517.527-20.985 2.475-38.634 16.896-44.984 37.02-6.456 20.017-.538 42.078 15.067 56.284 6.35 5.703 14.958 12.483 25.074 20.231 51.333 39.28 137.211 105.034 137.211 259.571 0 85.448-1.399 354.273-1.399 354.273l107.617.539s1.399-269.257 1.399-354.812c0-141.838-56.499-229.976-110.845-285.937 210.39-48.427 431.65-182.517 431.65-604.696" fill-rule="evenodd"/>
                     </svg>
@@ -156,14 +156,14 @@ function elementArticle(title, img, demo, codigo) {
 export function elementFormContact() {
     return `
         <form class="w-5/6 flex bg-white/2 backdrop-blur-sm flex-col gap-2 p-4 rounded-md relative lg:w-3/5 contact__form" action="https://formspree.io/f/mvzpbarl" method="POST">
-            <label class="form__text-name" for="text">Nombre</label>
+            <label class="lg:text-lg" for="text">Nombre</label>
             <input class="w-3/4 p-1 text-sm outline-none border-1 border-green-400 rounded-sm hover:border-green-dark focus:border-green-dark focus:bg-white/2 focus:backdrop-blur-sm form__input-text" type="text" name="nombre" id="text" required></input>
-            <label class="p-1 form__text-email" for="email">Email</label>
+            <label class="p-1 lg:text-lg form__text-email" for="email">Email</label>
             <input class="w-3/4 p-1 text-sm outline-none border-1 border-green-400 rounded-sm hover:border-green-dark focus:valid:border-green-dark focus:invalid:border-red-500 focus:bg-white/2 focus:backdrop-blur-sm form__input-email" type="email" name="email" id="email" autocomplete="on" required></input>
-            <label class="form__text-message" for="message">Mensaje</label>
+            <label class="lg:text-lg form__text-message" for="message">Mensaje</label>
             <textarea class="w-5/6 p-1 text-sm outline-none border-1 border-green-400 rounded-sm hover:border-green-dark focus:border-green-dark focus:bg-white/2 focus:backdrop-blur-sm form__input-message" name="mensaje" id="message" rows="5" required></textarea>
             <div class="mt-4 flex justify-end px-2 form__button-container">
-                <button class="text-sm w-24 h-8 border-1 border-b-4 border-r-4 border-green-400 hover:cursor-pointer form__button" type="submit">Enviar</button>
+                <button class="text-sm w-24 h-8 border-1 border-b-4 border-r-4 border-green-400 hover:cursor-pointer lg:text-base lg:h-9 form__button" type="submit">Enviar</button>
             </div>
 
             <div class="size-8 absolute border-t-2 border-l-2 border-green-400 -top-2 -left-2 rounded-tl-md"></div>
@@ -180,15 +180,15 @@ export function footer() {
         <footer class="bg-white/2 backdrop-blur-sm px-6 py-8 mx-3 border-4 border-green-400 relative rounded-xl opacity-0 sm:mt-16 footer">
             <div class="sm:grid sm:place-items-center sm:grid-cols-3 footer__container">
                 <div class="my-3 sm:h-full sm:m-0 footer__phone">
-                    <h4 class="font-semibold footer__title">Teléfono</h4>
-                    <p class="text-sm sm:h-9 sm:flex sm:items-end footer__text">+57 304 301 5421</p>
+                    <h4 class="font-semibold lg:text-xl footer__title">Teléfono</h4>
+                    <p class="text-sm sm:h-9 sm:flex sm:items-end lg:text-base footer__text">+57 304 301 5421</p>
                 </div>
                 <div class="mb-3 sm:h-full sm:m-0 footer__phone">
-                    <h4 class="font-semibold  footer__title">Email</h4>
-                    <p class="text-sm sm:h-9 sm:flex sm:items-end footer__text">omardev332@gmail.com</p>
+                    <h4 class="font-semibold lg:text-xl footer__title">Email</h4>
+                    <p class="text-sm sm:h-9 sm:flex sm:items-end lg:text-lg footer__text">omardev332@gmail.com</p>
                 </div>
                 <div class="mb-3 footer__redes">
-                    <h4 class="font-semibold mb-1 footer__title">Redes</h4>
+                    <h4 class="font-semibold mb-1 lg:text-xl footer__title">Redes</h4>
                     <div class="flex gap-2">
                         <a href="https://www.instagram.com/omar_jr_05_02/" target="_blank" rel="noopener noreferrer">
                         <svg class="size-7 fill-green-dark hover:fill-green-400 footer__red" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Instagram</title><path d="M7.0301.084c-1.2768.0602-2.1487.264-2.911.5634-.7888.3075-1.4575.72-2.1228 1.3877-.6652.6677-1.075 1.3368-1.3802 2.127-.2954.7638-.4956 1.6365-.552 2.914-.0564 1.2775-.0689 1.6882-.0626 4.947.0062 3.2586.0206 3.6671.0825 4.9473.061 1.2765.264 2.1482.5635 2.9107.308.7889.72 1.4573 1.388 2.1228.6679.6655 1.3365 1.0743 2.1285 1.38.7632.295 1.6361.4961 2.9134.552 1.2773.056 1.6884.069 4.9462.0627 3.2578-.0062 3.668-.0207 4.9478-.0814 1.28-.0607 2.147-.2652 2.9098-.5633.7889-.3086 1.4578-.72 2.1228-1.3881.665-.6682 1.0745-1.3378 1.3795-2.1284.2957-.7632.4966-1.636.552-2.9124.056-1.2809.0692-1.6898.063-4.948-.0063-3.2583-.021-3.6668-.0817-4.9465-.0607-1.2797-.264-2.1487-.5633-2.9117-.3084-.7889-.72-1.4568-1.3876-2.1228C21.2982 1.33 20.628.9208 19.8378.6165 19.074.321 18.2017.1197 16.9244.0645 15.6471.0093 15.236-.005 11.977.0014 8.718.0076 8.31.0215 7.0301.0839m.1402 21.6932c-1.17-.0509-1.8053-.2453-2.2287-.408-.5606-.216-.96-.4771-1.3819-.895-.422-.4178-.6811-.8186-.9-1.378-.1644-.4234-.3624-1.058-.4171-2.228-.0595-1.2645-.072-1.6442-.079-4.848-.007-3.2037.0053-3.583.0607-4.848.05-1.169.2456-1.805.408-2.2282.216-.5613.4762-.96.895-1.3816.4188-.4217.8184-.6814 1.3783-.9003.423-.1651 1.0575-.3614 2.227-.4171 1.2655-.06 1.6447-.072 4.848-.079 3.2033-.007 3.5835.005 4.8495.0608 1.169.0508 1.8053.2445 2.228.408.5608.216.96.4754 1.3816.895.4217.4194.6816.8176.9005 1.3787.1653.4217.3617 1.056.4169 2.2263.0602 1.2655.0739 1.645.0796 4.848.0058 3.203-.0055 3.5834-.061 4.848-.051 1.17-.245 1.8055-.408 2.2294-.216.5604-.4763.96-.8954 1.3814-.419.4215-.8181.6811-1.3783.9-.4224.1649-1.0577.3617-2.2262.4174-1.2656.0595-1.6448.072-4.8493.079-3.2045.007-3.5825-.006-4.848-.0608M16.953 5.5864A1.44 1.44 0 1 0 18.39 4.144a1.44 1.44 0 0 0-1.437 1.4424M5.8385 12.012c.0067 3.4032 2.7706 6.1557 6.173 6.1493 3.4026-.0065 6.157-2.7701 6.1506-6.1733-.0065-3.4032-2.771-6.1565-6.174-6.1498-3.403.0067-6.156 2.771-6.1496 6.1738M8 12.0077a4 4 0 1 1 4.008 3.9921A3.9996 3.9996 0 0 1 8 12.0077"/></svg>
@@ -202,7 +202,7 @@ export function footer() {
                     </div>
                 </div>
                 <div class="flex text-xs mb-1 sm:col-span-3 sm:w-full sm:gap-6 sm:mt-4 footer__credits">
-                    <button popovertarget="info-privacidad" class="w-1/2 sm:text-end underline hover:cursor-pointer footer__credits-link">Política de Privacidad</button>
+                    <button popovertarget="info-privacidad" class="w-1/2 sm:text-end underline lg:text-sm hover:cursor-pointer footer__credits-link">Política de Privacidad</button>
 
                     <div id="info-privacidad" popover class="bg-white/2 backdrop-blur-md p-4 border-x-2 border-green-400 rounded-md inset-x-1/5 top-30 sm:inset-x-1/3 footer__credits-dialog">
                         <h2 class="text-lg text-green-dark text-center font-bold my-2 sm:text-xl credits__dialog-title">Política de Privacidad</h3>
@@ -211,7 +211,7 @@ export function footer() {
                         Al usar el formulario de la sección contacto, aceptas este flujo de comunicación técnica con <strong>Formspree</strong>. 
                         </p>
                     </div>
-                    <p class="w-1/2 footer__credits-text">© 2026 Hecho con ❤️</p>
+                    <p class="w-1/2 lg:text-sm footer__credits-text">© 2026 Hecho con ❤️</p>
                 </div>
             </div>
             
--- scripts/ui.js
+++ scripts/ui.js
@@ -22,10 +22,10 @@ const form = elementFormContact();
 export function welcome() {
     return `
         <section class="w-full h-svh flex justify-center items-center section-welcome">
-            <h1 class="text-green-400 sm:text-xl">@ThinkPad-T480<span class="text-white-green">:</span></h1>
-            <p class="text-green-dark ml-1 sm:text-xl">~</p>
-            <p class="mr-2 sm:text-xl sm:mr-3">$</p>
-            <p class="sm:text-xl written-text-js"></p>  
+            <h1 class="text-green-400 sm:text-2xl">@ThinkPad-T480<span class="text-white-green">:</span></h1>
+            <p class="text-green-dark ml-1 sm:text-2xl">~</p>
+            <p class="mr-2 sm:text-2xl sm:mr-3">$</p>
+            <p class="sm:text-2xl written-text-js"></p>  
         </section>
     `;
 };
@@ -54,44 +54,44 @@ function renderAboutMe() {
         <div class="w-2 h-1/3 absolute z-1 top-2 -right-1 bg-green-400"></div>
 
         <div class="w-1/2 h-7 absolute top-0 left-0 bg-green-400 text-black-bluish px-2 rounded-br-md">
-          <h1 class="text-lg font-bold pl-1 about__title">Información</h1>
+          <h1 class="text-lg font-bold pl-1 lg:text-xl about__title">Información</h1>
         </div>
         
 
         <ul class="mt-8 about__intro center">
           <li class="mb-2">
-            <h4 class="font-semibold">Nombre</h4>
+            <h4 class="font-semibold lg:text-lg">Nombre</h4>
             <div class="ml-2 flex items-center">
               <img src="./assets/arrow-2.svg" class="size-2"/>
-              <p class="text-sm ml-1">Omar Junior Rodríguez Pérez</p>
+              <p class="text-sm ml-1 lg:text-base">Omar Junior Rodríguez Pérez</p>
             </div>
           </li>
           <li class="mb-2">
-            <h4 class="font-semibold">Edad</h4>
+            <h4 class="font-semibold lg:text-lg">Edad</h4>
             <div class="ml-2 flex items-center">
               <img src="./assets/arrow-2.svg" class="size-2"/>
-              <p class="text-sm ml-1">23 Años</p>
+              <p class="text-sm ml-1 lg:text-base">23 Años</p>
             </div>
           </li>
           <li class="mb-2">
-            <h4 class="font-semibold">Ubicación</h4>
+            <h4 class="font-semibold lg:text-lg">Ubicación</h4>
             <div class="ml-2 flex items-center">
               <img src="./assets/arrow-2.svg" class="size-2"/>
-              <p class="text-sm ml-1">Barranquilla-Colombia</p>
+              <p class="text-sm ml-1 lg:text-base">Barranquilla-Colombia</p>
             </div>
           </li>
           <li class="mb-2">
-            <h4 class="font-semibold">Ocupación</h4>
+            <h4 class="font-semibold lg:text-lg">Ocupación</h4>
             <div class="ml-2 flex items-center">
               <img src="./assets/arrow-2.svg" class="size-2"/>
-              <p class="text-sm ml-1">Estudiante Autodidacta</p>
+              <p class="text-sm ml-1 lg:text-base">Estudiante Autodidacta</p>
             </div>
           </li>
           <li class="mb-2">
-            <h4 class="font-semibold">Enfoque</h4>
+            <h4 class="font-semibold lg:text-lg">Enfoque</h4>
             <div class="ml-2 flex items-center">
               <img src="./assets/arrow-2.svg" class="size-2"/>
-              <p class="text-sm ml-1">Desarrollo Web Frontend</p>
+              <p class="text-sm ml-1 lg:text-base">Desarrollo Web Frontend</p>
             </div>
           </li>
         </ul>
@@ -104,14 +104,14 @@ function renderValues() {
     const newValuesElements = valuesElements.join("");
 
     return `
-      <div class="bg-white/2 backdrop-blur-sm border-1 border-l-2 border-green-400 px-6 py-2 my-18 rounded-br-2xl rounded-tl-xl relative lg:w-5/6 lg:m-0">
+      <div class="bg-white/2 backdrop-blur-sm border-1 border-l-2 border-green-400 px-6 py-2 my-18 rounded-br-2xl rounded-tl-xl relative lg:w-5/6 lg:h-full lg:m-0">
         <div class="size-8 absolute z-1 -top-2 -right-2 border-t-1 border-r-1 border-green-400"></div>
         <div class="size-8 absolute -bottom-2 -left-2 border-b-1 border-l-1 border-green-400"></div>
         <div class="w-2 h-1/3 absolute z-1 bottom-2 -right-1 bg-green-400"></div>
         <div class="w-2 h-1/3 absolute z-1 top-2 -left-1 bg-green-400"></div>
 
         <div class="w-1/2 h-7 absolute top-0 right-0 bg-green-400 text-black-bluish px-2 rounded-bl-md">
-          <h2 class="text-end text-lg font-semibold pr-1 values__title">Habilidades</h2>
+          <h2 class="text-end text-lg font-semibold pr-1 lg:text-xl values__title">Habilidades</h2>
         </div>
         <ul class="mt-8 values__list">
           ${newValuesElements}
@@ -127,7 +127,7 @@ function renderSkills() {
     return `
       <div class="w-full bg-white/2 backdrop-blur-sm px-4 py-6 my-18 flex justify-center items-center border-1 border-green-400 rounded-md relative lg:w-5/6 lg:h-1/3 lg:m-0 skills">
         <div class="h-7 absolute top-0 inset-x-1/4 bg-green-400 text-black-bluish px-2 rounded-b-lg">
-          <h1 class="text-lg text-center font-bold pl-1 about__title">Herramientas</h1>
+          <h1 class="text-lg text-center font-bold pl-1 lg:text-xl about__title">Herramientas</h1>
         </div>
 
         <div class="grid grid-cols-3 place-items-center gap-4 mt-6 mb-2 sm:grid-cols-4 skills__list">
@@ -150,7 +150,7 @@ export function renderProjects() {
               <h1 class="text-2xl text-white-green font-bold absolute -bottom-1 pl-1 lg:text-4xl lg:bottom-0 about__title">Proyectos de Práctica</h1>
               <div class="w-5/6 h-3 absolute -bottom-[12px] bg-black-bluish border-x-1 border-b-1 border-green-400 rounded-b-full"></div>
             </div>
-            <p class="text-center p-4 mt-2 sm:mt-4 mb-6 projects__description">
+            <p class="text-center p-4 mt-2 mb-6 sm:mt-4 lg:text-lg projects__description">
               Cada proyecto representa el progreso de mi aprendizaje, implementando conceptos aprendidos, ya que siempre busco hacer código de calidad y fácil de mantener.
             </p>
             <div class="p-2 m-4 grid grid-cols-1 place-items-center gap-y-10 lg:grid-cols-2 lg:gap-y-14 lg:gap-x-0 projects__grid">
@@ -169,7 +169,7 @@ export function renderContact() {
               <h1 class="text-2xl text-white-green font-bold absolute -bottom-1 pl-1 lg:text-4xl sm:bottom-0 about__title">Contacto</h1>
               <div class="w-3/5 h-3 absolute -bottom-[12px] bg-black-bluish border-x-1 border-b-1 border-green-400 rounded-b-full"></div>
           </div>
-          <p class="text-center p-4 my-2 lg:mt-4 lg:mb-6 contact__description">
+          <p class="text-center p-4 my-2 lg:text-lg lg:mt-4 lg:mb-6 contact__description">
             Disponible para participar en proyectos de desarrollo web frontend y crear páginas web básicas.
           </p>
           <div class="grid place-items-center sm:m-4">
